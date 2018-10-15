0faa6880f8f75268f979f517a8eb52ff37a49937
Dovile <dovile.likaite@gmail.com>
2018-10-15 07:33:22 +0300

    Add responsive design

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,74 +1,84 @@
-console.log("Pavyko!")
+$(document).ready(function(){
+    // Animated Navigation Bar
+    $(function(){
+     var shrinkHeader = 100;
+      $(window).scroll(function() {
+        var scroll = getCurrentScroll();
+          if (scroll >= shrinkHeader) {
+               $('.float-navigation').addClass('shrink');
+            }
+            else {
+                $('.float-navigation').removeClass('shrink');
+            }
+      });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+        }
+    });
 
-// Animated Navigation Bar
-$(function(){
- var shrinkHeader = 100;
-  $(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if (scroll >= shrinkHeader) {
-           $('.float-navigation').addClass('shrink');
+    // FIRST CAROUSEL
+    $('.karusele1').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false, 
+        responsive:{
+            0:{
+                items:1
+            }
+        },
+        autoplay:false,
+        autoplayTimeout:4000, // kaip padaryti letesni transition?
+        autoplayHoverPause:false
+    });
+
+    // SECOND CAROUSEL IN THE NEWS SECTION
+    $('.karusele2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }                   
         }
-        else {
-            $('.float-navigation').removeClass('shrink');
+    });
+
+    // FIXED SCROLL-BACK-TO-TOP BUTTON
+    // A) When the user scrolls down 100px from the top of the document, fade in the button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100){
+            $('.myBtn').fadeIn(150);
+        } else {
+            $('.myBtn').fadeOut(150);
         }
-  });
-function getCurrentScroll() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-    }
+    });
+    // B) When the user clicks on the button, scroll to the top of the document
+    $('.myBtn').click(function(){
+        $('body, html').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // KEEP THE COLOR OF AN ANTUR WHEN SELECTED
+    $('.keep-color').on('click', function(){
+        $('.keep-color').removeClass('selected');
+        $(this).addClass('selected');
+    });
 });
 
-// FIRST CAROUSEL
-$('.kar').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:false, 
-    responsive:{
-        0:{
-            items:1
-        }
-    },
-    autoplay:true,
-    autoplayTimeout:4000, // kaip padaryti letesni transition?
-    autoplayHoverPause:false
-});
-
-// SECOND CAROUSEL IN THE NEWS SECTION
-$('.kar2').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true, 
-    responsive:{
-        0:{
-            items:3
-        }
-    }
-});
-
-
-// FIXED Scroll Back To Top Button
-// When the user scrolls down 100px from the top of the document, fade in the button
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100){
-        $('.myBtn').fadeIn(150);
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
     } else {
-        $('.myBtn').fadeOut(150);
+        x.className = "topnav";
     }
-});
-
-// When the user clicks on the button, scroll to the top of the document
-$('.myBtn').click(function(){
-    $('body, html').animate({
-        scrollTop: 0
-    }, 500);
-});
-
-// Keep color of an anture when selected
-$('.keep-color').on('click', function(){
-    $('.keep-color').removeClass('selected');
-    $(this).addClass('selected');
-});
-
-
-
+}
